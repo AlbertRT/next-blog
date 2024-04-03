03537d205452d689a43a941a380dd0590dbc0a6a
Albert <email.albert22@gmail.com>
2024-04-04 06:01:33 +0700

    Inital Setup

---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+export type UserType = {
+	id: number;
+	node_id: string;
+	username: string;
+	email: string;
+	password: string;
+};
+
+export type PostType = {
+	id: number;
+	node_id: string;
+	post_id: string;
+	user_id: string;
+	title: string;
+	content: string;
+	created_at: Date;
+};
+
+export type CommentsType = {
+	id: number;
+	node_id: string;
+	post_id: string;
+	user_id: string;
+	content: string;
+	created_at: Date;
+};
